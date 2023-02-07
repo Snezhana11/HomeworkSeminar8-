f2d27b0b95aa25cfef7efee4ecf75dfa8934d455
Snezhana <alot3602@gmail.com>
2023-02-08 01:00:23 +0300

    task58

---
+++ task58/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число столбцов 1-й матрицы (и строк 2-й)");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int p= Convert.ToInt32(Console.ReadLine());
+
+int[,] firstMartrix = Generate2DArray(m, n);
+
+Console.WriteLine($"Первая матрица:");
+Print2DArray(firstMartrix);
+
+int[,] secomdMartrix = Generate2DArray(n, p);
+
+Console.WriteLine($"Вторая матрица:");
+Print2DArray(secomdMartrix);
+
+int[,] resultMatrix = Generate2DArray(m,p);
+
+СompositionMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+Print2DArray(resultMatrix);
+
+void СompositionMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
